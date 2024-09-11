05ba4544b48e01af2aa6a9c3bface6b1f56e50c3
gezerlavan <dev.talelias@gmail.com>
2024-09-11 15:05:44 +0300

    Implement renderBooks

--- js/book.controller.js
+++ js/book.controller.js
@@ -5,5 +5,21 @@ function onInit() {
 }
 
 function renderBooks() {
-  
+  const elTable = document.querySelector('tbody')
+  const books = getBooks()
+
+  const strHtmls = books.map(
+    book => `
+    <tr>
+      <td>${book.title}</td>
+      <td>${book.price}</td>
+      <td>
+        <button onclick="onReadBook(${book.id})">Read</button>
+        <button onclick="onUpdateBook(${book.id})">Update</button>
+        <button onclick="onDeleteBook(${book.id})">Delete</button>
+      </td>
+    </tr>`
+  )
+
+  elTable.innerHTML = strHtmls.join('')
 }
--- js/book.service.js
+++ js/book.service.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const gBooks = [
+  { id: 101, title: 'Book 1', price: 110, imgUrl: '' },
+  { id: 102, title: 'Book 2', price: 120, imgUrl: '' },
+  { id: 103, title: 'Book 3', price: 130, imgUrl: '' },
+]
+
+function getBooks() {
+  return gBooks
+}
