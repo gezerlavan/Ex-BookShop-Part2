3b8cc95881891e300f24500a8c49a4a97b300178
gezerlavan <dev.talelias@gmail.com>
2024-09-12 15:54:41 +0300

    Add local storage support

--- js/book.service.js
+++ js/book.service.js
@@ -1,28 +1,12 @@
 'use strict'
 
-const imgUrl =
+const BOOKS_KEY = 'books'
+
+const IMG_URL =
   'https://i.pinimg.com/736x/35/a4/52/35a45204888e5df66d047bb84a6f3ab6.jpg'
-  
-const gBooks = [
-  {
-    id: '101',
-    title: 'Book 1',
-    price: 110,
-    imgUrl,
-  },
-  {
-    id: '102',
-    title: 'Book 2',
-    price: 120,
-    imgUrl,
-  },
-  {
-    id: '103',
-    title: 'Book 3',
-    price: 130,
-    imgUrl,
-  },
-]
+
+var gBooks = []
+_createBooks()
 
 function getBooks() {
   return gBooks
@@ -31,16 +15,22 @@ function getBooks() {
 function removeBook(bookId) {
   const bookIdx = gBooks.findIndex(book => book.id === bookId)
   gBooks.splice(bookIdx, 1)
+
+  _saveBooks()
 }
 
 function updatePrice(bookId, newPrice) {
   const bookToUpdate = gBooks.find(book => book.id === bookId)
   bookToUpdate.price = newPrice
+
+  _saveBooks()
 }
 
 function addBook(title, price) {
   const newBook = _createBook(title, price)
   gBooks.push(newBook)
+
+  _saveBooks()
 }
 
 function getBookById(bookId) {
@@ -48,11 +38,28 @@ function getBookById(bookId) {
   return book
 }
 
-function _createBook(title = '', price = 100) {
+function _createBooks() {
+  gBooks = loadFromStorage(BOOKS_KEY)
+
+  if (gBooks && gBooks.length) return
+
+  gBooks = []
+  gBooks.push(_createBook('Book 1', 110))
+  gBooks.push(_createBook('Book 2', 120))
+  gBooks.push(_createBook('Book 3', 130))
+
+  _saveBooks()
+}
+
+function _createBook(title = 'Demo Book', price = 100) {
   return {
     id: makeId(),
     title,
     price,
-    imgUrl: '',
+    imgUrl: IMG_URL,
   }
+}
+
+function _saveBooks() {
+  saveToStorage(BOOKS_KEY, gBooks)
 }
