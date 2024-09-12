db771b9e1ba88423bb0ebe633cab6e45ee38c89e
gezerlavan <dev.talelias@gmail.com>
2024-09-12 15:28:05 +0300

    Add book details modal

--- js/book.controller.js
+++ js/book.controller.js
@@ -10,18 +10,20 @@ function renderBooks() {
 
   const strHtmls = books.map(
     book => `
-    <tr>
-      <td>${book.title}</td>
-      <td>${book.price}</td>
-      <td>
-        <button onclick="onReadBook('${book.id}')">Read</button>
-        <button onclick="onUpdateBook('${book.id}')">Update</button>
-        <button onclick="onRemoveBook('${book.id}')">Delete</button>
-      </td>
-    </tr>`
+      <tr>
+        <td>${book.title}</td>
+        <td>${book.price}</td>
+        <td>
+          <button onclick="onReadBook(event, '${book.id}')">Read</button>
+          <button onclick="onUpdateBook('${book.id}')">Update</button>
+          <button onclick="onRemoveBook('${book.id}')">Delete</button>
+        </td>
+      </tr>`
   )
 
-  elTable.innerHTML = strHtmls.join('')
+  elTable.innerHTML = !books.length
+    ? '<tr><td colspan="3">No books to show</td></tr>'
+    : strHtmls.join('')
 }
 
 function onRemoveBook(bookId) {
@@ -31,6 +33,7 @@ function onRemoveBook(bookId) {
 
 function onUpdateBook(bookId) {
   const newPrice = +prompt('Enter the new price')
+  if (!newPrice) return
   updatePrice(bookId, newPrice)
   renderBooks()
 }
@@ -40,4 +43,53 @@ function onAddBook() {
   const price = +prompt('Enter the price for the book')
   addBook(title, price)
   renderBooks()
+}
+
+function onReadBook(ev, bookId) {
+  const book = getBookById(bookId)
+  renderModal(ev, book)
+}
+
+function renderModal(ev, book) {
+  ev.stopPropagation()
+
+  const elDialog = document.querySelector('dialog')
+  const elDialogContainer = document.querySelector('dialog .dialog-container')
+
+  var strHtml = `
+  <h1>${book.title}</h1>
+  <p>Price: ${book.price}</p>
+  <img src="${book.imgUrl}" alt="book image">
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, qui!</p>
+  <button onclick="onCloseModal()">Close</button>
+  `
+
+  // var strHtml = `
+  //   <pre>${JSON.stringify(book)}</pre>
+  // `
+
+  elDialogContainer.innerHTML = strHtml
+  elDialog.showModal()
+
+  window.addEventListener('click', closeOnOutsideClick)
+}
+
+function onCloseModal() {
+  const elDialog = document.querySelector('dialog')
+  elDialog.close()
+
+  window.removeEventListener('click', closeOnOutsideClick)
+}
+
+function closeOnOutsideClick(ev) {
+  const elDialogContainer = document.querySelector('dialog .dialog-container')
+
+  const rect = elDialogContainer.getBoundingClientRect()
+  const isOutsideDialog =
+    ev.clientX < rect.left ||
+    ev.clientX > rect.right ||
+    ev.clientY < rect.top ||
+    ev.clientY > rect.bottom
+
+  if (isOutsideDialog) onCloseModal()
 }
--- js/book.service.js
+++ js/book.service.js
@@ -1,9 +1,27 @@
 'use strict'
 
+const imgUrl =
+  'https://i.pinimg.com/736x/35/a4/52/35a45204888e5df66d047bb84a6f3ab6.jpg'
+  
 const gBooks = [
-  { id: '101', title: 'Book 1', price: 110, imgUrl: '' },
-  { id: '102', title: 'Book 2', price: 120, imgUrl: '' },
-  { id: '103', title: 'Book 3', price: 130, imgUrl: '' },
+  {
+    id: '101',
+    title: 'Book 1',
+    price: 110,
+    imgUrl,
+  },
+  {
+    id: '102',
+    title: 'Book 2',
+    price: 120,
+    imgUrl,
+  },
+  {
+    id: '103',
+    title: 'Book 3',
+    price: 130,
+    imgUrl,
+  },
 ]
 
 function getBooks() {
@@ -23,6 +41,11 @@ function updatePrice(bookId, newPrice) {
 function addBook(title, price) {
   const newBook = _createBook(title, price)
   gBooks.push(newBook)
+}
+
+function getBookById(bookId) {
+  const book = gBooks.find(book => book.id === bookId)
+  return book
 }
 
 function _createBook(title = '', price = 100) {
